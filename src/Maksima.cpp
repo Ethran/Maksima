fdea1e8a7c8fd896c598c213e628509f6a11324b
Wiktor Wichrowski <wiktorw39@gmail.com>
2021-07-04 18:16:15 +0200

    first commit

---
+++ src/Maksima.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Maksima.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "function_maxima.h"
+
+int main() {
+	int a;
+	a = 9;
+	++a;
+	fun();
+	return 0;
+}
---
+++ src/function_maxima.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+
+int fun() {
+	printf("%dhello", 1);
+	return 0;
+}
